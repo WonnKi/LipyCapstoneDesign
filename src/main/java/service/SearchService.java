ff08ed40fb06b4157e3996aa0c3c834211867662
won <yang2159@kyungmin.ac.kr>
2024-04-15 22:49:53 +0900

    24/04/15 검색 service, controller 구현중

--- src/main/java/service/SearchService.java
+++ src/main/java/service/SearchService.java
@@ -4,14 +4,7 @@ import dto.SearchDto;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
-
-import java.net.URI;
-import java.nio.charset.StandardCharsets;
-
 @Service
 public class SearchService {
 
@@ -23,17 +16,9 @@ public class SearchService {
 
     private final String SEARCH_URL = "https://openapi.naver.com/v1/search/book.json?display=20";
 
-    public SearchDto search(String keyword, int start){
-        RestTemplate restTemplate = new RestTemplate();
-        HttpEntity<String> httpEntity = getHttpEntity();
-        URI targetUrl = UriComponentsBuilder
-                .fromUriString(SEARCH_URL)
-                .queryParam("query", keyword)
-                .queryParam("start", start)
-                .build()
-                .encode(StandardCharsets.UTF_8)
-                .toUri();
-        return restTemplate.exchange(targetUrl, HttpMethod.GET, httpEntity, SearchDto.class).getBody();
+    public SearchDto search(){
+        // 구현해야 함
+        return null;
     }
 
     private HttpEntity<String> getHttpEntity() { //헤더에 인증 정보 추가
