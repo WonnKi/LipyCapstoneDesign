a3d749cda330473e9d4c591dab590a7e77b9139f
UltimateLee <qkrqld4@naver.com>
2024-10-15 18:10:14 +0900

    게시글 작성자 오류 해결

--- src/main/frontend/src/screens/SocialingDetail.js
+++ src/main/frontend/src/screens/SocialingDetail.js
@@ -37,7 +37,7 @@ const SocialingDetails = () => {
                         Authorization: `Bearer ${token}`,
                     },
                 });
-                const loggedInName = userResponse.data.username;
+                const loggedInName = userResponse.data.Nickname;
                 setIsAuthor(postResponse.data.writer === loggedInName);
 
                 const applyResponse = await axios.get(`http://localhost:8080/${id}/isApplied`, {
@@ -76,6 +76,7 @@ const SocialingDetails = () => {
                         Authorization: `Bearer ${token}`,
                     },
                 });
+
                 setApplicants(response.data);
                 setLoading(false);
             } catch (error) {
@@ -264,8 +265,8 @@ const SocialingDetails = () => {
                         <Spinner an imation="border" />
                     ) : (
                         <ul>
-                            {applicants.map((applicants, index) => (
-                                <li key={index}>{applicants.name} - {applicants.email}</li>
+                            {applicants.map((applicant, index) => (
+                                <li key={index}>{applicant}</li> // applicant 자체가 문자열이므로 그대로 출력
                             ))}
                         </ul>
                     )}
