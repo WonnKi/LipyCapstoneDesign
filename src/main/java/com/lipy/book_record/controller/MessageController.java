8736b5158bc92da15e4e5821ad7eab1d100a3436
won <yang2159@kyungmin.ac.kr>
2024-10-31 11:36:31 +0900

    24/10/31 쪽지 기능 구현 완료 초안

--- src/main/java/com/lipy/book_record/controller/MessageController.java
+++ src/main/java/com/lipy/book_record/controller/MessageController.java
@@ -5,23 +5,55 @@ import com.lipy.book_record.entity.Member;
 import com.lipy.book_record.repository.MemberRepository;
 import com.lipy.book_record.service.MessageService;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RequiredArgsConstructor
-@RestController
+@RestController("/messages")
 public class MessageController {
     private final MessageService messageService;
     private final MemberRepository memberRepository;
 
-    public ResponseEntity<String> sendMessage(@RequestBody MessageDto messageDto){
-        Member member = memberRepository.findById(null).orElseThrow(()->{
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/send/{id}")
+    public ResponseEntity<String> sendMessage(@RequestBody MessageDto messageDto, @PathVariable("id")UUID id){
+        Member member = memberRepository.findById(id).orElseThrow(()->{
             return new IllegalArgumentException("유저를 찾을 수 없습니다.");
         });
         messageDto.setSenderName(member.getUsername());
+        messageService.write(messageDto);
 
-        return null;
+        return ResponseEntity.ok("쪽지를 보냈습니다.");
+    }
+
+    @GetMapping("/received/{id}")
+    public List<MessageDto> getReceivedMessage(@PathVariable("id") UUID id){
+        Member member = memberRepository.findById(id).orElseThrow(()->{
+            return new IllegalArgumentException("유저를 찾을 수 없습니다.");
+        });
+        return messageService.receivedMessage(member);
+    }
+
+    @DeleteMapping("received/{userId}/{messageId}")
+    public ResponseEntity<String> deleteReceivedMessage(@PathVariable("userId") UUID uId, @PathVariable("messageId") int mId){
+        Member member = memberRepository.findById(uId).orElseThrow(()->{
+            return new IllegalArgumentException("유저를 찾을 수 없습니다.");
+        });
+        messageService.deleteMessageByReceiver(mId,member);
+
+        return ResponseEntity.ok("Message Id : " + mId + " 쪽지를 삭제했습니다.");
+    }
+    @DeleteMapping("/sent/{userId}/{messageId}")
+    public ResponseEntity<String> deleteSentMessage(@PathVariable("userId") UUID uId, @PathVariable("messageId") int mId){
+        Member member = memberRepository.findById(uId).orElseThrow(()->{
+            return new IllegalArgumentException("유저를 찾을 수 없습니다.");
+        });
+        messageService.deleteMessageBySender(mId,member);
+
+        return ResponseEntity.ok("Message Id : " + mId + " 쪽지를 삭제했습니다.");
     }
 }
